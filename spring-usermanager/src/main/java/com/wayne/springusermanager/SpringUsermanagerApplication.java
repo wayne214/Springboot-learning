2b0bedb3e2589e266d40ceeed0d031e58d40642e
wayne <jun_wei_like1@163.com>
2019-02-15 17:08:56 +0800

    Spring Data JPA 和 Thymeleaf 综合实践

---
+++ spring-usermanager/src/main/java/com/wayne/springusermanager/SpringUsermanagerApplication.java
@@ -0,0 +1,14 @@
+package com.wayne.springusermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUsermanagerApplication.class, args);
+    }
+
+}
+
