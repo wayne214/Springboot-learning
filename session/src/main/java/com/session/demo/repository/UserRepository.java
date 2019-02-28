4f7613490bbc6630c703da51e056e296ce06833c
wayne <jun_wei_like1@163.com>
2019-02-28 16:48:32 +0800

    redis实现session共享

---
+++ session/src/main/java/com/session/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.session.demo.repository;
+
+import com.session.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserName(String userName);
+}
