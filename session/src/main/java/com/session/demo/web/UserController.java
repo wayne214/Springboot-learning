17c73465d706bfcbcca148c07e6c926dc07f3d4b
wayne <jun_wei_like1@163.com>
2019-03-01 10:10:27 +0800

    springboot使用Cache缓存

--- session/src/main/java/com/session/demo/web/UserController.java
+++ session/src/main/java/com/session/demo/web/UserController.java
@@ -3,6 +3,7 @@ package com.session.demo.web;
 import com.session.demo.model.User;
 import com.session.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,28 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @RequestMapping("/hello")
+    @Cacheable(value = "helloCache")
+    public String hello(String name) {
+        System.out.println("没有走缓存！");
+        return "hello" + name;
+    }
+
+    /**
+     * @Cacheable 支持如下几个参数:
+     * value：缓存的名称。
+     * key：缓存的 key，可以为空，如果指定要按照 SpEL 表达式编写；如果不指定，则缺省按照方法的所有参数进行组合。
+     * condition：触发条件，只有满足条件的情况才会加入缓存，默认为空，既表示全部都加入缓存，支持 SpEL。
+     * @param name
+     * @return
+     */
+    @RequestMapping("/condition")
+    @Cacheable(value="condition",condition="#name.length() <= 4")
+    public String condition(String name) {
+        System.out.println("没有走缓存！");
+        return "hello "+name;
+    }
 
     @RequestMapping(value = "/setSession")
     public Map<String, Object> setSession (HttpServletRequest request) {
