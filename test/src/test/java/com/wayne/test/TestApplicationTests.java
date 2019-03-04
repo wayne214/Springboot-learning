8890fda9b396f770486497604d7e5ca404646c44
wayne <jun_wei_like1@163.com>
2019-03-04 18:01:45 +0800

    spring-boot测试对JUnit的使用

--- test/src/test/java/com/wayne/test/TestApplicationTests.java
+++ test/src/test/java/com/wayne/test/TestApplicationTests.java
@@ -3,9 +3,8 @@ package com.wayne.test;
 import com.wayne.test.controller.HelloController;
 import com.wayne.test.services.HelloService;
 import com.wayne.test.services.HelloServiceImpl;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import com.wayne.test.utils.Calculation;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.rule.OutputCapture;
@@ -20,6 +19,7 @@ import javax.annotation.Resource;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -61,6 +61,59 @@ public class TestApplicationTests {
                 .accept(MediaType.APPLICATION_JSON_UTF8))
 //                .andDo(print());
                 .andExpect(content().string(containsString("hello")));
+    }
+
+    //JUnit使用
+    Calculation calculation = new Calculation();
+    int result;     //测试结果
+    //在 JUnit 4 中使用 @Test 标注为测试方法
+    @Test
+    public void testAdd() {
+        System.out.println("-----testAdd开始测试-----");
+        result = calculation.add(1,3);
+        assertEquals(4, result);
+        System.out.println("-----testAdd正常运行结束----");
+    }
+
+
+    //又一个测试方法
+    //timeout 表示测试允许的执行时间毫秒数，expected 表示忽略哪些抛出的异常（不会因为该异常导致测试不通过）
+    @Test(timeout = 1, expected = NullPointerException.class)
+    public void testSub() {
+        System.out.println("---testSub开始测试---");
+
+        result = calculation.sub(3, 2);
+        assertEquals(1, result);
+
+        throw new NullPointerException();
+
+        //System.out.println("---testSub正常运行结束---");
+    }
+
+    //指示该[静态方法]将在该类的[所有]测试方法执行之[前]执行
+    @BeforeClass
+    public static void beforeAll() {
+        System.out.println("||==BeforeClass==||");
+        System.out.println("||==通常在这个方法中加载资源==||");
+    }
+
+    //指示该[静态方法]将在该类的[所有]测试方法执行之[后]执行
+    @AfterClass
+    public static void afterAll() {
+        System.out.println("||==AfterClass==||");
+        System.out.println("||==通常在这个方法中释放资源==||");
+    }
+
+    //该[成员方法]在[每个]测试方法执行之[前]执行
+    @Before
+    public void beforeEvery() {
+        System.out.println("|==Before==|");
+    }
+
+    //该[成员方法]在[每个]测试方法执行之[后]执行
+    @After
+    public void afterEvery() {
+        System.out.println("|==After==|");
     }
 
 }
