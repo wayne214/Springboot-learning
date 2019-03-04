bb67f346389bad8c5b65b57a720e864d2a0024aa
wayne <jun_wei_like1@163.com>
2019-03-04 17:11:40 +0800

    spring boot admin server监控

---
+++ adminclient/src/main/java/com/wayne/adminclient/AdminclientApplication.java
@@ -0,0 +1,13 @@
+package com.wayne.adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminclientApplication.class, args);
+    }
+
+}
