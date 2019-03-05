bfa0e6c7b349201b77d18330990e6a4aad195899
wayne <jun_wei_like1@163.com>
2019-03-05 11:19:08 +0800

    spring boot部署项目

---
+++ packageh/src/main/java/com/wayne/packageh/PackagehApplication.java
@@ -0,0 +1,13 @@
+package com.wayne.packageh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackagehApplication.class, args);
+    }
+
+}
